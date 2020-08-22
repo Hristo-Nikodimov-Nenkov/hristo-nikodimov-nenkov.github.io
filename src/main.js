09c3841775b1c4710b8cf8ea30bb644f3824475c
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-08-22 17:11:43 +0200

    .

--- src/main.js
+++ src/main.js
@@ -5,17 +5,9 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-const baseUrl = "https://hristo.nikodimov.nenkov.github.io/";
 
 new Vue({
   router,
   store,
   render: h => h(App),
-  created () {
-    if (sessionStorage.redirect) {
-      const redirect = sessionStorage.redirect.split(baseUrl)[1];
-      delete sessionStorage.redirect
-      this.$router.push(redirect)
-    }
-  }
 }).$mount('#app')
