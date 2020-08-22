eaccfa3d9c525c34c9d9c57dc331eaf2516a7b63
Hristo-Nikodimov-Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-08-22 13:59:47 +0000

    Deploying to gh-pages from  @ efb1666efa9c6f95f8965e3f3104ba01e7565107 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04753fbd4dd175491df457594c4c2016.js"
+  "/precache-manifest.5ca7e3a326550c59019e2e6765e9a48f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "gh-pages-app"});
