a5bacef02ed7f954fa1972db93628cbe86a8ffc8
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-08-23 13:47:01 +0200

    FixAttempt - Redirect to requested url using vue router.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,18 @@ import store from './store'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App),
+    created() {
+        let redirect = sessionStorage.redirect;
+        delete sessionStorage.redirect;
+
+        const baseUrl = 'https://hristo-nikodimov-nenkov.github.io'
+        if (redirect && redirect !== location.href) {
+            redirect = redirect.split(baseUrl)[1];
+            this.$router.push(redirect)
+                .then(() => console.log('Redirecting to requested url'))
+        }
+    }
 }).$mount('#app')
