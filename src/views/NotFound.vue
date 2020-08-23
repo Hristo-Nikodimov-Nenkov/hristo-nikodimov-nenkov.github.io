73f1cd4d80b26cee3601f0e532522e5288a933dd
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-08-23 13:10:27 +0200

    Added - NotFound.vue page with auto redirect to home page.

---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+ <h1>Page not found.</h1>
+  <h5>Auto redirect to home page in 5 seconds.</h5>
+</div>
+</template>
+<script>
+export default {
+  name:"NotFound",
+  created() {
+    setTimeout(()=>{
+      this.$router.push("/")
+      .then(() => console.log('Auto redirect to Home page.'))
+    }, 5000)
+  }
+}
+</script>
+<style scoped>
+  h1, h5{
+    text-align: center;
+  }
+</style>
