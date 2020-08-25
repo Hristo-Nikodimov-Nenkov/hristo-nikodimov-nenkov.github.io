f0049e310f0deaaa7e40ce0399adf1f869e66264
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-08-25 18:00:23 +0200

    - Added - store - navigation module - getters - getSection.

--- src/store/navigation.js
+++ src/store/navigation.js
@@ -2,11 +2,26 @@ export default {
     namespaced: true,
     state: {
         mergeSectionsItems: true,
-        sections: []
+        sections: [
+            {
+                name: 'Home',
+                items: [
+                    {
+                        text: 'Home',
+                        url: '/'
+                    },
+                    {
+                        text: 'About',
+                        url: '/about'
+                    }
+                ]
+            }
+        ]
     },
     getters: {
         getMergeSectionsItems: (state) => state.mergeSectionsItems,
-        getSections:(state) => state.sections
+        getSections: (state) => state.sections,
+        getSection: (state) => (sectionName) => state.sections.find(s => s.name === sectionName)
     },
     mutations: {
         setMergeSectionsItems: (state, value) => state.mergeSectionsItems = value,
