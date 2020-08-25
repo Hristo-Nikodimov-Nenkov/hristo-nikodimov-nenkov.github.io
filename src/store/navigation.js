b5fc3249efa7b22294ab7cecf05ba9b575f435fe
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-08-25 13:40:55 +0200

    - Added - navigation module.

--- src/store/navigation.js
+++ src/store/navigation.js
@@ -1,7 +1,31 @@
 export default {
-    namespaced:true,
-    state: {},
-    getters: {},
-    mutations: {},
-    actions: {}
+    namespaced: true,
+    state: {
+        sections: []
+    },
+    getters: {
+        getSections: (state) => state.sections,
+        getSection: (state) => (name) => state.sections[name]
+    },
+    mutations: {
+        setSections: (state, sections) => state.sections = sections,
+        setSection: (state, name, value) => state.sections[name] = value
+    },
+    actions: {
+        loadSections({commit}, sections) {
+            commit('setSections', sections);
+        },
+        loadSectionsFromStorage({commit}) {
+            const sectionsStr = sessionStorage.getItem('navSections') ?? '[]';
+            const sections = JSON.parse(sectionsStr);
+
+            commit('setSections', sections);
+        },
+        saveSectionsToStorage({state}) {
+            sessionStorage.setItem('navSections', JSON.stringify(state.sections));
+        },
+        updateSection({commit}, section) {
+            commit('setSection', section.name, section.value);
+        }
+    }
 }
