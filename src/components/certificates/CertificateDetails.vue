3521c9cc8134487e30934ecfbdd0e3831ead8f03
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-08-28 10:50:29 +0200

    - Added - QR Code component.

--- src/components/certificates/CertificateDetails.vue
+++ src/components/certificates/CertificateDetails.vue
@@ -1,12 +1,31 @@
 <template>
-<div>
-
-</div>
+  <div class="container">
+    <img :src="this.certificateThumb" alt="Certificate preview image.">
+    <div class="">{{ this.courseName }}</div>
+    <div class="">{{ this.grade }}</div>
+    <qrcode-vue :value="this.url"/>
+  </div>
 </template>
 
 <script>
+import QrcodeVue from 'qrcode.vue';
+
 export default {
-name: "CertificateDetails"
+  name: "CertificateDetails",
+  components: {
+    QrcodeVue
+  },
+  data: function () {
+    return {
+      url: ""
+    }
+  },
+  props: {
+    courseName: String,
+    certificateThumb: String,
+    grade: Number,
+    verificationUrl: String
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,17 @@
   <div id="home-page">
     <h1>Portfolio</h1>
     <h1>Hristo Nikodimov Nenkov</h1>
+    <certificate-details verification-url="https://softuni.bg"/>
   </div>
 </template>
 
 <script>
+import CertificateDetails from "@/components/certificates/CertificateDetails";
+
 export default {
   name: 'Home',
   components: {
+    CertificateDetails
   }
 }
 </script>
