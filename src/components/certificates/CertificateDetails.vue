e0c63c0b5180fdc2d78efce19f8360da384503e7
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-09-30 11:35:11 +0200

    - CertificateDetails design - Ready.

--- src/components/certificates/CertificateDetails.vue
+++ src/components/certificates/CertificateDetails.vue
@@ -1,15 +1,70 @@
 <template>
-<div>
-Certificate with Id: {{this.$route.params.id}}
-</div>
+  <div id="details">
+    <div id="thumb">
+      <img :src="getCertificateUrl" alt="Certificate preview image">
+    </div>
+    <div id="info">
+      <div id="course-name">  Course: {{getCertificate.courseName}}</div>
+      <div id="issued-on">Issued On: {{ getCertificate.issuedOn }}</div>
+      <div id="summary">
+        {{getCertificate.summary}}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import {createNamespacedHelpers} from "vuex";
+
+const {mapState, mapActions} = createNamespacedHelpers('certificates');
+
 export default {
-name: "CertificateDetails"
+  name: "CertificateDetails",
+  components:{
+  },
+  computed: {
+    ...mapState(['certificates']),
+    getCertificate:function (){
+      return this.certificates.find(c => c.id === this.$route.params.id)
+    },
+    getCertificateUrl:function (){
+      return `https://softuni.bg/certificates/certificates/converttoimage/${this.getCertificate.id}?code=${this.getCertificate.code}`
+    }
+  },
+  methods: {
+    ...mapActions(["loadCertificates"])
+  },
+  created() {
+    this.certificate = this.certificates.find(c => c.id === this.$route.params.id)
+  }
 }
 </script>
 
 <style scoped>
+#details {
+  display: grid;
+  grid-template-areas: "thumb info";
+  grid-template-columns: 2fr 4fr;
+}
 
+#thumb {
+  grid-area: thumb;
+}
+
+#thumb>img{
+  width: max-content;
+  max-width: 90%;
+  border: 1px solid #555555;
+  border-radius: 7px;
+  margin: 5px;
+  padding: 5px;
+}
+
+#info {
+  grid-area: info;
+  padding-top: 4vh;
+}
+#info>div{
+  margin: 1vh 0;
+}
 </style>
