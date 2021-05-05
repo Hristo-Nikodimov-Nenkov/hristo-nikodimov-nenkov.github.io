5ad239fc9ff4898546ec1a49e5470032476b7750
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-05-05 20:30:02 +0200

    - Added - Contacts page.

---
+++ src/views/Contacts.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+   <h1 class="text-center font-italic font-weight-bold">This is contacts page.</h1>
+</div>
+</template>
+
+<script>
+export default {
+   name: "Contacts"
+}
+</script>
+
+<style scoped>
+
+</style>
