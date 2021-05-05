ae95ec38a44e7eca81e14b09cd663eb9619a9fb5
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-05-05 20:52:30 +0200

    - Added - AppFooter component.

---
+++ src/components/common/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="text-center font-italic">
+   Vue 2 application hosted on GitHub Pages.
+</div>
+</template>
+
+<script>
+export default {
+   name: "AppFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
