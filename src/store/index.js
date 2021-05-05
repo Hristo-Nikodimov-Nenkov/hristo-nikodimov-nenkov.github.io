2628d03dfc4fd597e119be8b2fa065a9538944d0
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-05-05 18:03:17 +0200

    - Added - navigation module to store.

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+import navigation from "@/store/navigation";
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {},
+    mutations: {},
+    actions: {},
+    modules: {
+        navigation
+    }
 })
