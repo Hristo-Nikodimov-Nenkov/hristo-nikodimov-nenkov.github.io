de4159f679b0dbd8e1bd19dd67558e41cb531a46
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-05-06 15:15:38 +0200

    - Added - contacts module.

---
+++ src/store/contacts.js
@@ -0,0 +1,22 @@
+export default {
+    namespaced: true,
+    state:{
+        contacts:[
+            {
+                text:"E-Mail",
+                value:"hristo.nikodimov.nenkov@gmail.com",
+                icon:"fa fa-envelope"
+            },
+            {
+                text:"Phone Number",
+                value: "",
+                icon:"fa fa-phone"
+            },
+            {
+                text:"Discord",
+                value:"",
+                icon:""
+            }
+        ]
+    }
+}
