47818b1f7cf8fb907d2ae3ae64ce77d06817b6bb
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-05-06 14:23:58 +0200

    - Added - lorem paragraphs for content.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,30 @@
 <template>
   <div id="home-page">
+     <h1>Home page.</h1>
+     <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi delectus laudantium magni
+        maxime necessitatibus qui quis, repudiandae soluta. Ducimus et impedit molestias nam nihil
+        obcaecati, sapiente sunt unde velit?
+     </p>
+     <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias assumenda consectetur
+        cumque debitis doloribus, ea eum fugiat incidunt itaque iure laudantium molestias,
+        nemo non officiis possimus quibusdam quisquam rem vero.
+     </p>
+     <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda autem eius eos eveniet,
+        facere harum illo iusto repellat saepe! Aperiam aut dolorem est exercitationem fugit magnam sit unde vitae!
+     </p>
+     <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis deleniti magni molestiae
+        mollitia praesentium quasi voluptatum? Beatae debitis dicta eius, est labore magnam natus quaerat
+        repellendus, similique, suscipit tempore voluptate?
+     </p>
+     <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores culpa dolor expedita fugiat
+        quam similique vel voluptatum? Accusantium consectetur consequatur, deserunt error eum exercitationem,
+        explicabo in libero nisi ratione, vero.
+     </p>
   </div>
 </template>
 
