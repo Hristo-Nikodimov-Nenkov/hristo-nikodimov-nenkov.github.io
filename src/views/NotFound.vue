f97a73731adbce573a1bae33a0f0755ae6f0397d
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-05-07 16:42:30 +0200

    - Fixed - page changes after 3 seconds even after route change to valid.

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,44 +1,40 @@
 <template>
-  <div>
-    <h1>Page not found.</h1>
-    <h5>Auto redirect to home page in {{ this.secondsLeft }} seconds.</h5>
-  </div>
+   <div>
+      <h1>Page not found.</h1>
+      <h5>Auto redirect to home page in {{ this.secondsLeft }} seconds.</h5>
+   </div>
 </template>
 
 <script>
 const countDownStartValue = 3;
 
 export default {
-  name: "NotFound",
-  data: function () {
-    return {
-      secondsLeft: countDownStartValue
-    }
-  },
-  methods: {
-    countDownTimer() {
-      if (this.secondsLeft < 1) {
-        this.$router.push('/')
-            .then(() => {
-              console.log('Auto redirect to Home page.');
-            })
-            .catch((err) => console.log(err));
-      } else {
-        setTimeout(() => {
-          this.secondsLeft--
-          this.countDownTimer()
-        }, 1000);
+   name: "NotFound",
+   data: function () {
+      return {
+         secondsLeft: countDownStartValue,
       }
-    }
-  },
-  created() {
-    this.countDownTimer();
-  }
+   },
+   methods: {
+      countDownTimer: async function () {
+         while (this.secondsLeft > 0) {
+            await new Promise(r => setTimeout(r, 1000));
+            this.secondsLeft--
+         }
+
+         if (this.$route.name === "NotFound") {
+            await this.$router.push("/")
+         }
+      },
+   },
+   created() {
+      this.countDownTimer();
+   }
 }
 </script>
 
 <style scoped>
 h1, h5 {
-  text-align: center;
+   text-align: center;
 }
 </style>
