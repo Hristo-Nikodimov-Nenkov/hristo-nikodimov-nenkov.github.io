8160f5b947e284e9de1bc86dd5c591ac93825589
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-05-07 16:45:27 +0200

    - Added - Education page to views

---
+++ src/views/Education.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+      <Sections/>
+   </div>
+</template>
+
+<script>
+import Sections from "@/components/education/Sections";
+
+export default {
+   name: "Education",
+   components: {
+      Sections
+   }
+}
+</script>
+
+<style scoped>
+
+</style>
