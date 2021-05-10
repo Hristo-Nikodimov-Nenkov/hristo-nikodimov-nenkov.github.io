8b37c37f424bffaa98ef726fea14d3f216f4e7fa
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-05-10 12:18:18 +0200

    - Added - CourseCertificate, CourseDetails, Courses, Project, ProjectDetails & Projects components.

---
+++ src/components/projects/Project.vue
@@ -0,0 +1,14 @@
+<template>
+   <div>
+   </div>
+</template>
+
+<script>
+export default {
+   name: "Project"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/projects/ProjectDetails.vue
@@ -0,0 +1,14 @@
+<template>
+   <div>
+   </div>
+</template>
+
+<script>
+export default {
+   name: "ProjectDetails"
+}
+</script>
+
+<style scoped>
+
+</style>
