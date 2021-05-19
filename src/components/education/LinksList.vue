d89fe8576ff24e5a20181b36595f95b4b35e70df
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-05-19 18:26:33 +0200

    - Added - LinksList component

---
+++ src/components/education/LinksList.vue
@@ -0,0 +1,25 @@
+<template>
+   <ul class="list-inline">
+      <li
+         v-for="(link, index) of links"
+         :key="index">
+         <router-link
+            :to="link.url">
+            {{ link.text }}
+         </router-link>
+      </li>
+   </ul>
+</template>
+
+<script>
+export default {
+   name: "LinksList",
+   props: {
+      links: Array
+   }
+}
+</script>
+
+<style scoped>
+
+</style>
