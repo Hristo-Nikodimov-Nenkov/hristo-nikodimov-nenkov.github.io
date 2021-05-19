a3f9222c3687d0cdf8044cfd2108615a967a0e48
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-05-19 18:26:54 +0200

    - Added - LinksList component to EducationPage view.

--- src/views/education/EducationPage.vue
+++ src/views/education/EducationPage.vue
@@ -1,11 +1,26 @@
 <template>
-   <router-view/>
+   <div class="d-flex">
+      <links-list
+         class="d-inline-block px-2"
+         :links="getModulesAsLinks"/>
+      <router-view class="d-inline-block px-3"/>
+   </div>
 </template>
 
 <script>
+import LinksList from "@/components/education/LinksList";
+import {createNamespacedHelpers} from "vuex";
+
+const {mapGetters} = createNamespacedHelpers("education");
 
 export default {
-   name: "EducationPage"
+   name: "EducationPage",
+   components: {
+      LinksList
+   },
+   computed: {
+      ...mapGetters(["getModulesAsLinks"])
+   }
 }
 </script>
 
